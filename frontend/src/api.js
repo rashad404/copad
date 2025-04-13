b2e65635e0a4c1537500066b49e575b7d85d749b
Rashad Mirza <rashad.mirza.404@gmail.com>
2025-04-13 17:46:01 -0500

    err fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 // Get the current domain
 const getBaseUrl = () => {
-  if (import.meta.env.DEV) {
-    return "http://localhost:8080/api";
+  // Use the environment variable if it exists
+  if (import.meta.env.VITE_API_URL) {
+    return import.meta.env.VITE_API_URL;
   }
   
-  // In production, use the same domain as the frontend
+  // Fallback to the current domain
   const protocol = window.location.protocol;
   const hostname = window.location.hostname;
   return `${protocol}//${hostname}/api`;
