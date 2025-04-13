18d661b6bed811b97cb0531da41d1d59488dd9e5
Rashad Mirza <rashad.mirza.404@gmail.com>
2025-04-13 18:53:17 -0500

    err fix

--- backend/src/main/java/com/drcopad/copad/config/SecurityConfig.java
+++ backend/src/main/java/com/drcopad/copad/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
             )
             .addFilterBefore(jwtFilter, OAuth2LoginAuthenticationFilter.class)
             .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .sessionFixation().migrateSession()
             )
             .requestCache().disable();
