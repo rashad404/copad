449846cebaa636cd4727484cf2d92fe34c18b1d5
Rashad Mirza <rashad.mirza.404@gmail.com>
2025-04-16 23:57:19 -0500

    changes

--- frontend/src/components/GuestChat.jsx
+++ frontend/src/components/GuestChat.jsx
@@ -178,7 +178,7 @@ export default function GuestChat() {
   return (
     <div className="flex flex-col h-full">
       {/* Chat messages */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-4 pb-24" style={{ maxHeight: 'calc(100vh - 120px)' }}>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4" style={{ maxHeight: 'calc(500px - 120px)' }}>
         {messages.map((message, index) => (
           <div
             key={index}
@@ -234,9 +234,8 @@ export default function GuestChat() {
         </div>
       )}
 
-      {/* Message input (stick to bottom) */}
-      <div className="sticky bottom-0 left-0 right-0 z-10 p-4 border-t bg-white
-                      pb-[env(safe-area-inset-bottom)]">
+      {/* Message input */}
+      <div className="p-4 border-t bg-white">
         <div className="flex items-center gap-2">
           <button
             onClick={handleSaveChat}
